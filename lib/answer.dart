8c3cdeac68b5a7a936ee0d6f56815e8f94803962
Kieren-Foenander <kfoenander98@gmail.com>
2022-02-14 22:06:14 +1000

    built question app handled if run out of options

---
+++ lib/answer.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+
+  Answer(this.selectHandler, this.answerText);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.all(5),
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
